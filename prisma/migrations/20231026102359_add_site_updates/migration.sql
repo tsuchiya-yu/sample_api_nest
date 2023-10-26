977ca2b5e59a85f2a280d9990daf590ceb0f3eb7
tsuchiya-yu <nomoreheronomorehero@gmail.com>
2023-10-26 19:25:47 +0900

    siteUpdatesテーブルのマイグレーションファイル作成

---
+++ prisma/migrations/20231026102359_add_site_updates/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `siteUpdates` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `content` VARCHAR(191) NOT NULL,
+    `publishedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- RenameIndex
+ALTER TABLE `users` RENAME INDEX `email` TO `users_email_key`;
