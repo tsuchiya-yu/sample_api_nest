8f7a11454ea0edee067713237ae3e3d023e86d71
tsuchiya-yu <nomoreheronomorehero@gmail.com>
2023-12-15 22:53:01 +0900

    fix typo

--- src/shop-mst/shop-mst.resolver.ts
+++ src/shop-mst/shop-mst.resolver.ts
@@ -2,14 +2,14 @@ import { Query, Resolver, Args, Int } from '@nestjs/graphql';
 import { ShopMst } from './shop-mst'
 import { ShopMstService } from './shop-mst.service';
 import { HttpException, HttpStatus } from '@nestjs/common';
-import { FindManyshopMstArgs } from 'src/@generated/prisma-nestjs-graphql/shop-mst/find-manyshop-mst.args';
+import { FindManyShopMstArgs } from 'src/@generated/prisma-nestjs-graphql/shop-mst/find-many-shop-mst.args';
 
 @Resolver()
 export class ShopMstResolver {
     constructor(private readonly shopMstService: ShopMstService) {}
 
     @Query(() => [ShopMst], { nullable: true })
-    async shopMsts(@Args() args: FindManyshopMstArgs): Promise<ShopMst[]> {
+    async shopMsts(@Args() args: FindManyShopMstArgs): Promise<ShopMst[]> {
         console.log('call: shoMsts#ShopMstResolver');
         const shopMsts = await this.shopMstService.findAll(args);
         if (!shopMsts) {
--- src/shop-mst/shop-mst.service.ts
+++ src/shop-mst/shop-mst.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { ShopMst } from './shop-mst'
-import { FindManyshopMstArgs } from 'src/@generated/prisma-nestjs-graphql/shop-mst/find-manyshop-mst.args';
+import { FindManyShopMstArgs } from 'src/@generated/prisma-nestjs-graphql/shop-mst/find-many-shop-mst.args';
 
 @Injectable()
 export class ShopMstService {
     constructor(private readonly prisma: PrismaService) { }
 
-    async findAll(args: FindManyshopMstArgs): Promise<ShopMst[]> {
+    async findAll(args: FindManyShopMstArgs): Promise<ShopMst[]> {
         return this.prisma.shopMst.findMany(args);
     }
 }
