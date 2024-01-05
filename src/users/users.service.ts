31e64a4b637ab08b6bc4c4999446ba0bd7d57b16
tsuchiya-yu <nomoreheronomorehero@gmail.com>
2024-01-06 02:18:52 +0900

    add S3へのファイルアップロード処理

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,10 +6,24 @@ import { CreateOneUserArgs } from 'src/@generated/prisma-nestjs-graphql/user/cre
 import { FindUniqueUserArgs } from 'src/@generated/prisma-nestjs-graphql/user/find-unique-user.args';
 import { UserUpdateInput } from 'src/@generated/prisma-nestjs-graphql/user/user-update.input';
 import type { FileUpload } from "graphql-upload/processRequest.mjs";
+import * as AWS from 'aws-sdk';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UsersService {
-    constructor(private readonly prisma: PrismaService) { }
+    private s3: AWS.S3;
+
+    constructor(private readonly prisma: PrismaService, private readonly configService: ConfigService) {
+        // AWS設定
+        AWS.config.update({
+            accessKeyId: configService.get<string>('AWS_ACCESS_KEY_ID'),
+            secretAccessKey: configService.get<string>('AWS_SECRET_ACCESS_KEY'),
+            region: configService.get<string>('AWS_REGION'),
+        });
+
+        // S3インスタンスの作成
+        this.s3 = new AWS.S3();
+    }
 
     async findFirst(args: FindFirstUserArgs): Promise<User | null> {
         return this.prisma.user.findFirst({
@@ -47,15 +61,33 @@ export class UsersService {
         });
     }
 
-    async processUpload(userId: number, file: FileUpload): Promise<boolean> {
-        // TODO: 要実装
-        if (process.env.NODE_ENV === 'production') {
-            // 本番：S3にアップロード
-        } else {
-            // 開発：ローカルに保存
+    async processUpload(userId: number, upload: FileUpload): Promise<boolean> {
+        // TODO: 
+        // 　　・ファイルのディレクトリ構成を考える
+        // 　　・S3の処理は別の部分で共通化したい
+        // 　　・DBにファイルのパスを保存する処理を実装する
+        const file = upload[0].file;
+        const createReadStream = file.createReadStream;
+        const readStreams = createReadStream(); 
+        const filename = file.filename;
+        
+        const bucketName = this.configService.get<string>('AWS_S3_BUCKET_NAME');
+        const key = `${Date.now()}-${filename}`; // ファイル名
+
+        const params = {
+          Bucket: bucketName,
+          Key: key,
+          Body: readStreams,
+        };
+      
+        try {
+          await this.s3.upload(params).promise();
+          return true;
+        } catch (error) {
+          console.error('S3 upload error:', error);
+          return new Promise(function(resolve, reject) {
+            reject(true);
+          });
         }
-        return new Promise(function(resolve, reject) {
-            resolve(true);
-        });
     }
 }
